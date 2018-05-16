27e1e5d0afe27e5226bf21721ef64281dbd9ba0f
sethsacher <seth.sacher@nav-labs.com>
2018-05-16 08:37:20 -0400

    Starting fullpage migration

--- js/main.js
+++ js/main.js
@@ -11,15 +11,72 @@
 /*=========================================================================
 	Pagepiling Active
 =========================================================================*/
-    $('#pagepiling').pagepiling({
-        direction: 'vertical',
+    $('#fullpage').fullpage({
+        //Navigation
         menu: '#menu',
-        anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'careers', 'page7'],
-        sectionsColor: ['#fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#fff'],
-        navigation: {
-            'position': 'right',
-            'tooltips': ['Home', 'About', 'Delivery', 'Leadership', 'Testimonials', 'Downloads', 'Careers', 'Contact Us']
-        },
+        lockAnchors: false,
+        anchors:['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'careers', 'page7'],
+        navigation: false,
+        navigationPosition: 'right',
+        navigationTooltips: ['Home', 'About', 'Delivery', 'Leadership', 'Testimonials', 'Downloads', 'Careers', 'Contact Us'],
+        showActiveTooltip: false,
+        slidesNavigation: false,
+        slidesNavPosition: 'bottom',
+
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        continuousHorizontal: false,
+        scrollHorizontally: false,
+        interlockedSlides: false,
+        dragAndMove: false,
+        offsetSections: false,
+        resetSliders: false,
+        fadingEffect: false,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        scrollOverflowReset: false,
+        scrollOverflowOptions: null,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+        bigSectionsDestination: null,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
+
+        //Design
+        controlArrows: true,
+        verticalCentered: true,
+        sectionsColor : ['#fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#fff'],
+        paddingTop: '3em',
+        paddingBottom: '10px',
+        fixedElements: '#header, .footer',
+        responsiveWidth: 0,
+        responsiveHeight: 0,
+        responsiveSlides: false,
+        parallax: false,
+        parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        lazyLoading: true,
+
+        //events
+        // onLeave: function(index, nextIndex, direction){},
         afterRender: function(){
             $('#pp-nav').addClass('custom');
         },
@@ -30,6 +87,10 @@
                 $('#pp-nav').addClass('custom');
             }
         }
+        // afterResize: function(){},
+        // afterResponsive: function(isResponsive){},
+        // afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        // onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
     });
 
   /*=========================================================================
